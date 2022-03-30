218d1697fd1f63b9fb00d5365407e030a5ddbb09
Guilherme Magno <guilhermertfmagno@gmail.com>
2022-03-29 22:54:58 -0300

    Finish project

---
+++ index.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const {URL} = require("url");
+
+const bodyParser = require("./helpers/bodyParser");
+const routes = require("./routes");
+
+const server = http.createServer((request, response) => {
+    const parsedUrl = new URL(`http://${request.headers.host}${request.url}`)
+    
+    console.log(`Request Method: ${request.method} | Endpoint: ${parsedUrl.pathname}`)
+    
+    let {pathname} = parsedUrl;
+    let id = null;
+    
+    const splitEndpoint = pathname.split('/').filter((routeItem) => Boolean(routeItem));
+    
+    if (splitEndpoint.length > 1) {
+        pathname = `/${splitEndpoint[0]}/:id`;
+        id = splitEndpoint[1];
+    }
+    
+    const route = routes.find((route) =>
+      route.endpoint === pathname && route.method === request.method
+    )
+    
+    if (route) {
+        request.query = Object.fromEntries(parsedUrl.searchParams);
+        request.params = {id};
+        
+        response.send = (statusCode, body) => {
+            response.writeHead(statusCode, {"Content-Type": "application/json"});
+            response.end(JSON.stringify(body))
+        }
+        
+        if (["POST", "PUT"].includes(request.method)) {
+            bodyParser(request, () => {
+                route.handler(request, response)
+            });
+        } else {
+            route.handler(request, response);
+        }
+    } else {
+        response.writeHead(404, {"Content-Type": "text/html"});
+        response.end(`Cannot ${request.method} ${request.url}`)
+    }
+});
+
+server.listen(3000, () => console.log("Server started at http://localhost:3000"));
---
+++ mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Guilherme"
+    },
+    {
+        id: 2,
+        name: "Carlos"
+    },
+    {
+        id: 3,
+        name: "Fred"
+    },
+]
